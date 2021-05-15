c51f652f81df0224f0f366169be82482e5cd5f16
Brandon Edgehill <brandonedgehill@gmail.com>
2021-05-15 02:54:54 -0400

    Creation of working item service module

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,97 +1,21 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-
-const fakeDB = [
-    
-    {
-        name:"Shiny Pebble",
-        rarity:"common",
-        description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
-        goldPerTurn:0
-    },
-
-    {
-        name:"Shiny peach",
-        rarity:"common",
-        description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
-        goldPerTurn:0
-    },
-
-    {
-        name:"Shiny pumpkin",
-        rarity:"common",
-        description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
-        goldPerTurn:0
-    }
-
-]
+const itemServices = require("../services/itemsService")
 
 //View a Single Item
-router.get("/:item_name",(req,res)=>{
-
-    const name = req.params.item_name;
-    
-    const itemFound = fakeDB.find(item=>item.name === name);
-    
-    if(itemFound != undefined)
-    {
-        res.json({
-            message: `Details of product with the id ${itemFound.name}}`,
-            data: itemFound
-        })
-    }
-
-    else
-    {
-        res.status(404).json({
-            message : `Product with id ${name} was not found`
-        })
-    }
-    
-})
+router.get("/:item_name",itemServices.getSingleItem)
 
 //Add a New Item
-router.post("/items",(req,res)=>{
-
-    res.json(
-        {
-            message:"Post request to add an item is working."
-        }
-    )
-    
-})
+router.post("/items",itemServices.postSingleItem)
 
 //Delete an Item
-router.delete("/:item_name",(req,res)=>{
+router.delete("/:item_name",itemServices.deleteSingleItem)
 
-    res.json(
-        {
-            message:"Delete request to delete an item is working."
-        }
-    )
-    
-})
 //Update an Item
-router.put("/:item_id",(req,res)=>{
-
-    res.json(
-        {
-            message:"Put request to update an item is working."
-        }
-    )
-    
-})
+router.put("/:item_id",itemServices.putSingleItem)
 
 //View A List of Items
-router.get("/",(req,res)=>{
-
-    res.json(
-        {
-            data:fakeDB
-        }
-    )
-    
-})
+router.get("/",itemServices.getAllItems)
 
 
 module.exports = router
---
+++ services/itemsService.js
@@ -0,0 +1,48 @@
+exports.getSingleItem = (req,res)=>{
+
+    res.json(
+        {
+            message:"Get request to retrieve an item is working."
+        }
+    )
+
+};
+
+exports.postSingleItem = (req,res)=>{
+
+    res.json(
+        {
+            message:"Post request to add an item is working."
+        }
+    )
+
+};
+
+exports.deleteSingleItem = (req,res)=>{
+
+    res.json(
+        {
+            message:"Delete request to delete an item is working."
+        }
+    )
+
+};
+
+exports.putSingleItem = (req,res)=>{
+
+    res.json(
+        {
+            message:"Put request to update an item is working."
+        }
+    )
+
+};
+exports.getAllItems = (req,res)=>{
+
+    res.json(
+        {
+            message:"Get request to retrieve all items is working."
+        }
+    )
+
+};
